b2d70299c4bc5ba01143316d148d98446c86c264
Ahsan Naeem <ahsannaeem150@gmail.com>
2023-11-09 16:51:28 +0500

    Add selectors in JS + Delete unnecessary images
    Selected all Btns and labels using JS selectors
    Remove one png file which was not needed

---
+++ calculatorScript.js
@@ -0,0 +1,28 @@
+const inputText = document.querySelector('.input-top');
+const signText = document.querySelector('.signTxt');
+const calculatedText = document.querySelector('.output-bottom');
+
+const clearBtn = document.querySelector('.clearBtn');
+const deleteBtn = document.querySelector('.deleteBtn');
+const sevenBtn = document.querySelector('.sevenBtn');
+const eightBtn = document.querySelector('.eightBtn');
+const nineBtn = document.querySelector('.nineBtn');
+const divideBtn = document.querySelector('.divideBtn');
+const fourBtn = document.querySelector('.fourBtn');
+const fiveBtn = document.querySelector('.fiveBtn');
+const sixBtn = document.querySelector('.sixBtn');
+const multiplyBtn = document.querySelector('.multiplyBtn');
+const oneBtn = document.querySelector('.oneBtn');
+const twoBtn = document.querySelector('.twoBtn');
+const threeBtn = document.querySelector('.threeBtn');
+const minusBtn = document.querySelector('.minusBtn');
+const pointBtn = document.querySelector('.pointBtn');
+const zeroBtn = document.querySelector('.zeroBtn');
+const equalBtn = document.querySelector('.equalBtn');
+const plusBtn = document.querySelector('.plusBtn');
+
+
+
+
+
+
