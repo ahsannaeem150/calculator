93bd3912864aab47ccf86f0576ea46b6413549f7
Ahsan Naeem <ahsannaeem150@gmail.com>
2023-11-09 19:01:12 +0500

    Add event listeners to all buttons except = btn

--- calculatorScript.js
+++ calculatorScript.js
@@ -22,7 +22,103 @@ const equalBtn = document.querySelector('.equalBtn');
 const plusBtn = document.querySelector('.plusBtn');
 
 
+inputText.textContent = '';
+signText.textContent = '';
+calculatedText.textContent = '';
+
+let [numberOne, operator, numberTwo] = ['', '', ''];
 
 
+
+
+
+sevenBtn.addEventListener('click', () => {
+    inputText.textContent += '7';
+});
+eightBtn.addEventListener('click', () => {
+    inputText.textContent += '8';
+});
+nineBtn.addEventListener('click', () => {
+    inputText.textContent += '9';
+});
+fourBtn.addEventListener('click', () => {
+    inputText.textContent += '4';
+});
+fiveBtn.addEventListener('click', () => {
+    inputText.textContent += '5';
+});
+sixBtn.addEventListener('click', () => {
+    inputText.textContent += '6';
+});
+oneBtn.addEventListener('click', () => {
+    inputText.textContent += '1';
+});
+twoBtn.addEventListener('click', () => {
+    inputText.textContent += '2';
+});
+threeBtn.addEventListener('click', () => {
+    inputText.textContent += '3';
+});
+pointBtn.addEventListener('click', () => {
+    if (inputText.textContent == '') {
+        inputText.textContent = '0.';
+    } else {
+        inputText.textContent += '.';
+    }
+})
+zeroBtn.addEventListener('click', () => {
+    inputText.textContent += '0';
+});
+
+
+
+divideBtn.addEventListener('click', () => {
+    operator = ' ÷ ';
+    if (inputText.textContent == '') {
+        inputText.textContent = '0 ÷ ';
+    } else {
+        inputText.textContent += ' ÷ ';
+    }
+});
+multiplyBtn.addEventListener('click', () => {
+    operator = ' x ';
+    if (inputText.textContent == '') {
+        inputText.textContent = '0 x ';
+    } else {
+        inputText.textContent += ' x ';
+    }
+});
+minusBtn.addEventListener('click', () => {
+    operator = ' - ';
+    if (inputText.textContent == '') {
+        inputText.textContent = '0 - ';
+    } else {
+        inputText.textContent += ' - ';
+    }
+});
+plusBtn.addEventListener('click', () => {
+    operator = ' + ';
+    if (inputText.textContent == '') {
+        inputText.textContent = '0 + ';
+    } else {
+        inputText.textContent += ' + ';
+    }
+});
+
+
+
+clearBtn.addEventListener('click', () => {
+    inputText.textContent = '';
+    calculatedText.textContent = '';
+    signText.textContent = '';
+    [numberOne, operator, numberTwo] = ['', '', ''];
+});
+deleteBtn.addEventListener('click', () => {
+    if (inputText.textContent[inputText.textContent.length - 1] == ' ') {
+        inputText.textContent = inputText.textContent.slice(0, inputText.textContent.length - 2);
+    } else {
+        inputText.textContent = inputText.textContent.slice(0, inputText.textContent.length - 1);
+    }
+});
 
 
